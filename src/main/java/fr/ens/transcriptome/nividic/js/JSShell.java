63df90cae50d8f61ce0616f8e32b7dbd454171e3
Laurent Jourdren <jourdren@biologie.ens.fr>
2007-05-07 12:36:02 +0000

    Add javascript shell class
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@202 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/nividic/js/JSShell.java
@@ -0,0 +1,286 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.nividic.js;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+import fr.ens.transcriptome.nividic.util.SystemUtils;
+
+/**
+ * This class define a shell wrapper for javascript.
+ * @author Laurent Jourdren
+ */
+public class JSShell {
+
+  private JavaScriptEngine engine;
+  private ScriptLoader scriptLoader;
+  private static final String JAVASCRIPT_PATH = "/javascript";
+
+  /**
+   * Define class to load script from javascript.
+   * @author Laurent Jourdren
+   */
+  private class ScriptLoader {
+
+    private JavaScriptEngine engineloader;
+
+    /**
+     * Load a script from a file.
+     * @param f File to load
+     * @throws FileNotFoundException if the file is not found
+     */
+    public void source(final File f) throws FileNotFoundException {
+
+      FileInputStream fis = new FileInputStream(f);
+      this.engineloader.eval(fis, f.getName());
+    }
+
+    /**
+     * Load a script from an inputStream.
+     * @param is InputStreamto use
+     * @param comment Comment about the stream
+     */
+    public void source(final InputStream is, final String comment) {
+
+      this.engineloader.eval(is, comment);
+    }
+
+    /**
+     * Launch an editor.
+     * @param f File to edit. It can be null to edit an empty file
+     */
+    public void edit(final File f) {
+
+      SystemUtils.launchEditor(f);
+    }
+
+    /**
+     * Execute a javascript command and return the time needed to execute a
+     * Javascript command.
+     * @param cmd command to execute
+     */
+    public void time(final String cmd) {
+
+      long start = System.currentTimeMillis();
+      this.engineloader.eval(cmd);
+      long end = System.currentTimeMillis();
+
+      System.out.println(end - start);
+    }
+
+    /**
+     * Execute a system command.
+     * @param cmd command to execute
+     * @throws IOException if an error occurs while executing the command
+     */
+    public void system(final String cmd) throws IOException {
+
+      Process p = Runtime.getRuntime().exec(cmd);
+
+      BufferedReader stdInput = new BufferedReader(new InputStreamReader(p
+          .getInputStream()));
+
+      BufferedReader errInput = new BufferedReader(new InputStreamReader(p
+          .getInputStream()));
+
+      // read the output from the command
+      String s = null;
+      while ((s = stdInput.readLine()) != null) {
+        System.out.println(s);
+      }
+
+      while ((s = errInput.readLine()) != null) {
+        System.err.println(s);
+      }
+
+    }
+
+    /**
+     * Get the current time in milliseconds since the Unix epoch.
+     * @return The current time in milliseconds since the Unix epoch
+     */
+    public long currentTimeMillis() {
+
+      return System.currentTimeMillis();
+    }
+
+    /**
+     * Returns the maximum amount of memory that the Java virtual machine will
+     * attempt to use.
+     * @return the maximum memory
+     */
+    public long maxMemory() {
+
+      Runtime rt = Runtime.getRuntime();
+
+      return rt.maxMemory();
+    }
+
+    /**
+     * Returns the total amount of memory in the Java virtual machine.
+     * @return the maximum memory
+     */
+    public long totalMemory() {
+
+      Runtime rt = Runtime.getRuntime();
+
+      return rt.totalMemory();
+    }
+
+    /**
+     * Returns the amount of free memory in the Java Virtual Machine.
+     * @return the maximum memory
+     */
+    public long freeMemory() {
+
+      Runtime rt = Runtime.getRuntime();
+
+      return rt.freeMemory();
+    }
+
+    /**
+     * Constructor
+     * @param engine The Javascript engine
+     */
+    public ScriptLoader(final JavaScriptEngine engine) {
+
+      this.engineloader = engine;
+    }
+  }
+
+  /**
+   * Create a javascript shell.
+   * @param in console input
+   * @param out console output
+   * @param err console error output
+   */
+  public void shell(final InputStream in, final PrintStream out,
+      final PrintStream err) {
+
+    this.engine.shell(in, out, err);
+  }
+
+  /**
+   * Eval a javascript command.
+   * @param cmd Command to evaluate
+   */
+  public void eval(final String cmd) {
+
+    this.engine.eval(cmd);
+  }
+
+  /**
+   * Load a script.
+   * @param file File to load
+   * @throws FileNotFoundException throws FileNotFoundException if cannot find
+   *           the script
+   */
+  public void source(final File file) throws FileNotFoundException {
+
+    this.scriptLoader.source(file);
+  }
+
+  /**
+   * Load a script from an inputStream.
+   * @param is InputStreamto use
+   * @param comment Comment about the stream
+   */
+  public void source(final InputStream is, final String comment)
+      throws FileNotFoundException {
+
+    this.scriptLoader.source(is, comment);
+  }
+
+  //
+  // Constructors
+  //
+
+  /**
+   * Public constructor.
+   * @param args command line arguments
+   */
+  public JSShell(final String[] args, final String javascriptPath) {
+
+    this.engine = new RhinoJavaScriptEngine();
+    // this.engine = new JSR223JavaScriptEngine();
+
+    System.out.println("Javascript engine: "
+        + this.engine.getClass().getSimpleName());
+
+    this.engine.init();
+    this.scriptLoader = new ScriptLoader(this.engine);
+
+    this.engine.addVariable("out", System.out);
+    this.engine.addVariable("fs", new FileSystem());
+    this.engine.addVariable("engine", this.scriptLoader);
+
+    // if (true) return;
+
+    if (args != null)
+      this.engine.addVariable("args", args);
+
+    for (int i = 0; i < Defaults.BUILTIN_SCRIPTS.length; i++) {
+
+        InputStream is =null;
+        
+      if (javascriptPath!=null) {
+          
+          String path = javascriptPath + "/" + Defaults.BUILTIN_SCRIPTS[i];
+          try {
+          is = new FileInputStream(path);
+          } catch(IOException e){
+              
+          }
+          
+      }
+      else {
+        //JAVASCRIPT_PATH
+      String path = JAVASCRIPT_PATH + "/" + Defaults.BUILTIN_SCRIPTS[i];
+
+      if (JSShell.class.getResource(path) != null)
+        is = JSShell.class.getResourceAsStream(path);
+      }
+      
+        if (is!=null)  this.engine.eval(is,
+            Defaults.BUILTIN_SCRIPTS[i]);
+        
+    }
+
+  }
+
+  /**
+   * Public constructor.
+   */
+  public JSShell() {
+
+    this(null, null);
+  }
+
+}
