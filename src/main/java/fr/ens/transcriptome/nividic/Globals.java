023971b74cbffda348ac6b9366475c5dba2c16a4
Laurent Jourdren <jourdren@biologie.ens.fr>
2007-05-10 12:18:06 +0000

    Change version of nividic
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@226 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/Globals.java
+++ src/main/java/fr/ens/transcriptome/nividic/Globals.java
@@ -47,7 +47,7 @@ public final class Globals {
   /** Name of the application. */
   public static final String APP_DESCRITPION = "A framework for DNA chips analysis";
   /** Version of the application. */
-  public static final String VERSION = "0.5";
+  public static final String VERSION = "0.6";
   /** Copyright. */
   // public static final String COPYRIGHT = "Copyright (c) 2003-2004 CEA - ENS";
   public static final String COPYRIGHT = "Copyright (c) 2004-2007 ENS Microarray platform";
