ebee68d4a6493246f61c5ac2d875ddd3352ec824
Laurent Jourdren <jourdren@biologie.ens.fr>
2007-08-13 12:07:04 +0000

    Update design package import
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@314 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/ExpressionMatrix.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/ExpressionMatrix.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
@@ -22,6 +22,7 @@
 
 package fr.ens.transcriptome.nividic.om;
 
+import fr.ens.transcriptome.nividic.om.design.Design;
 import fr.ens.transcriptome.nividic.om.filters.ExpressionMatrixFilter;
 import fr.ens.transcriptome.nividic.om.translators.Translator;
 
