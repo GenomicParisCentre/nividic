660c6aad5fda988a4dc865b2651b57d357ca80b9
Laurent Jourdren <jourdren@biologie.ens.fr>
2008-03-14 14:04:38 +0000

    Fix: Exception message
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@655 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/io/ExpressionMatrixWriter.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/io/ExpressionMatrixWriter.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
@@ -117,7 +117,7 @@ public abstract class ExpressionMatrixWriter {
     try {
       setOutputStream(new FileOutputStream(file));
     } catch (FileNotFoundException e) {
-      throw new NividicIOException("Error while reading file : "
+      throw new NividicIOException("Error while writing file : "
           + file.getName());
     }
 
