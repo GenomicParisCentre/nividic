ae55cc988d93d0d811cf9d5416a91bf6e7a94b14
Laurent Jourdren <jourdren@biologie.ens.fr>
2008-12-02 09:04:31 +0000

    Datasource commit.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@968 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/datasources/DataSourceUtils.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/datasources/DataSourceUtils.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
