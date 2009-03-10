cf4822316ccd94e234ee3315d6af2623c062a1a6
Laurent Jourdren <jourdren@biologie.ens.fr>
2009-03-10 14:59:05 +0000

    Add a new constructor in LimmaDesignReader to take the filename as argument
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@990 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/design/io/LimmaDesignReader.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/design/io/LimmaDesignReader.java
@@ -24,6 +24,8 @@ package fr.ens.transcriptome.nividic.om.design.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -282,6 +284,17 @@ public class LimmaDesignReader extends InputStreamDesignReader {
   public LimmaDesignReader(final InputStream is) throws NividicIOException {
 
     super(is);
+  }
+  
+  /**
+   * Public constructor
+   * @param filename File to read
+   * @throws NividicIOException if the stream is null
+   * @throws FileNotFoundException if the file doesn't exist
+   */
+  public LimmaDesignReader(final String filename) throws NividicIOException, FileNotFoundException {
+
+    this(new FileInputStream(filename));
   }
 
 }
