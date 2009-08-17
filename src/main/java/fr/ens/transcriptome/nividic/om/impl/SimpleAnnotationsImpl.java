922da42e09ab1950213a8ba09d22d5a091f670e4
Laurent Jourdren <jourdren@biologie.ens.fr>
2009-08-17 15:19:48 +0000

    Fix addProperties in SimpleAnnotationsImpl
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@1123 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/impl/SimpleAnnotationsImpl.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/impl/SimpleAnnotationsImpl.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
@@ -65,8 +65,9 @@ public class SimpleAnnotationsImpl implements Annotation, Serializable {
     if (annotations == null)
       return;
 
-    for (Map.Entry<String, String> e : this.data.entrySet())
-      setProperty(e.getKey(), e.getValue());
+    for (String key : annotations.keySet())
+      setProperty(key, annotations.getProperty(key));
+
   }
 
   /**
