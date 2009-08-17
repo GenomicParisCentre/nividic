13f671e7bd9b3d648a9c8bb6a483cc88cb9300c0
Laurent Jourdren <jourdren@biologie.ens.fr>
2009-08-17 14:50:26 +0000

    Remove unused import
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@1122 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/design/io/LimmaDesignWriter.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/design/io/LimmaDesignWriter.java
@@ -24,7 +24,6 @@ package fr.ens.transcriptome.nividic.om.design.io;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
