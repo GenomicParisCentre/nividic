01d54a7dc95aeced948783721809a7f49bce81b5
Laurent Jourdren <jourdren@biologie.ens.fr>
2009-09-01 15:08:01 +0000

    Fix bug in setMetaColumn in Spot class
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@1142 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/om/impl/SpotImpl.java
+++ src/main/java/fr/ens/transcriptome/nividic/om/impl/SpotImpl.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
@@ -730,7 +730,7 @@ public class SpotImpl implements Spot, Serializable {
       throws BioAssayRuntimeException {
 
     testIndex();
-    setLocation(BioAssayUtils.setMetaRow(getLocation(), metaColumn));
+    setLocation(BioAssayUtils.setMetaColumn(getLocation(), metaColumn));
   }
 
   /**
