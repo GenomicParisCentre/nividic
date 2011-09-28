6ea49d4c30377c89456919a659401d02a257f2de
Laurent Jourdren <jourdren@biologie.ens.fr>
2011-09-28 15:12:16 +0000

    TYPO: In Version.java
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/nividic@2093 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/nividic/util/Version.java
+++ src/main/java/fr/ens/transcriptome/nividic/util/Version.java
@@ -9,7 +9,7 @@
  *      http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
+ * of the ï¿½cole Normale Supï¿½rieure and the individual authors.
  * These should be listed in @author doc comments.
  *
  * For more information on the Nividic project and its aims,
@@ -154,13 +154,11 @@ public final class Version implements Comparable {
 
     final Version v = (Version) version;
 
-    final int compMajor = Integer.valueOf(getMajor()).compareTo(v
-        .getMajor());
+    final int compMajor = Integer.valueOf(getMajor()).compareTo(v.getMajor());
     if (compMajor != 0)
       return compMajor;
 
-    final int compMinor = Integer.valueOf(getMinor()).compareTo(v
-        .getMinor());
+    final int compMinor = Integer.valueOf(getMinor()).compareTo(v.getMinor());
     if (compMinor != 0)
       return compMinor;
 
@@ -189,8 +187,8 @@ public final class Version implements Comparable {
    */
   public int hashCode() {
 
-    HashCodeBuilder hcb = new HashCodeBuilder(HASHCODE_ODD_NUMBER_1,
-        HASHCODE_ODD_NUMBER_2);
+    HashCodeBuilder hcb =
+        new HashCodeBuilder(HASHCODE_ODD_NUMBER_1, HASHCODE_ODD_NUMBER_2);
 
     hcb.append(major).append(minor).append(revision);
 
